324d8d743f377afad1aecd6c36fe05f4370cfdc4
xgolis <xgolis@stuba.sk>
2023-05-22 01:46:50 +0200

    Add dockerfile

--- main.go
+++ main.go
@@ -9,16 +9,13 @@ import (
 )
 
 func main() {
-	// Create a new router
 	router := mux.NewRouter()
 
-	// Define the endpoint handler
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, "OK")
 	})
 
-	// Start the HTTP server
 	log.Println("Server listening on port 8080")
 	log.Fatal(http.ListenAndServe("0.0.0.0:8080", router))
 }
